01c0b634efa7f1a62caf0dc89737c3985720386d
Yabir <yabirgb@gmail.com>
2017-10-20 00:23:03 +0200

    Fix template type definition for methods
    The type to be returned by the methods was bad defined
    using templates.
    
    Also changed tabulation for public and private

--- reto3/myStack.h
+++ reto3/myStack.h
@@ -1,16 +1,18 @@
+#include <list>
+
 #ifndef _MY_STACK_H_
 #define _MY_STACK_H_
 
 template <class T>
 class myStack{
-private:
-  list<T> l;
-public:
+ private:
+  std::list<T> l;
+ public:
   myStack();
   ~myStack();
-  void push(<T> data);
-  <T> pop();
-  <T> peak();
+  void push( T data );
+  T pop();
+  T peak();
   bool isEmpty();
   int size();
 };
