d0171e7ae1def7a94196d1e9d3cd4ec8613380e9
Yabir <yabirgb@gmail.com>
2017-10-23 08:36:48 +0200

    Fixed heads of methods

--- reto3/myStack.cpp
+++ reto3/myStack.cpp
@@ -1,32 +1,34 @@
 #include <list>
 #include <cassert>
+#include "myStack.h"
 using namespace std;
 
 template <class T>
-myStack::myStack() { }
+myStack<T>::myStack() { }
 
 // Esto hace falta?  List ya tiene su propio destructor que la limpia, no?
-myStack::~myStack() {
+template <class T>
+myStack<T>::~myStack() {
   while (l.begin() != l.end()) {
     l.erase(l.begin());
   }
 }
 
 template <class T>
-void myStack::push (<T> data) {
+void myStack<T>::push (T data) {
   pushFront(data);
 }
 
 // Throws exception if isEmpty()
 template <class T>
-int myStack::peak() {
+T myStack<T>::peak() {
   assert(!isEmpty()); // Esto podríamos dejarlo más bonito
   return *l.front(); // Devolvemos el elemento apuntado por el iterador
 }
 
 // Throws exception if isEmpty()
 template <class T>
-int myStack::pop() {
+T myStack<T>::pop() {
   assert(!isEmpty()); // Esto podríamos dejarlo más bonito
   T aux = *l.front();
   l.erase(l.begin());
@@ -34,26 +36,11 @@ int myStack::pop() {
 }
 
 template <class T>
-bool myStack::isEmpty() {
+bool myStack<T>::isEmpty() {
   return l.empty();
 }
 
 template <class T>
-int myStack::size() {
+int myStack<T>::size() {
   return l.size();
-}
-
-// Este main hay que rehacerlo y llevarlo a otro archivo test.cpp
-int main() {
-  myStack S;
-  cout << "Peak: " << S.peak()  << " " << S.isEmpty() << endl;
-  cout << "Pop: " << S.pop() << " " << S.isEmpty() << endl;
-  S.push(1);
-  S.push(2);
-  S.push(3);
-  S.push(4);
-  S.push(5);
-  cout << "Peak: " << S.peak()  << " " << S.isEmpty() << endl;
-  cout << "Pop: " << S.pop() << " " << S.isEmpty() << endl;
-  cout << "Pop: " << S.pop() << " " << S.isEmpty() << endl;
 }
