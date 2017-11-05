4cf31c01d247972fc5ef65dc819cb8997e6dd621
Ocete <ocetebt@hotmail.com>
2017-11-05 14:48:26 +0100

    Actualizada preparacion para el parcial de ED

--- Preparación parcial de prácticas/intersectionWithoutThird.cpp
+++ Preparación parcial de prácticas/intersectionWithoutThird.cpp
@@ -15,6 +15,23 @@ set<int> intersectionWithoutThird(set<int> S1, set<int> S2, set<int> S3) {
   return result;
 }
 
+// This methods returns a set with the elements in 2 and only two of these sets.
+set<int> WeirdIntersection(set<int> S1, set<int> S2, set<int> S3) {
+  set<int> result;
+  set<int>::iterator it;
+  for (it = S1.begin(); it != S1.end(); it++) {
+    if (S2.count(*it) == 0 ^ S3.count(*it) == 0 ) {
+      result.insert(*it);
+    }
+  }
+  for (it = S2.begin(); it != S2.end(); it++) {
+    if (S1.count(*it) == 0 && S3.count(*it) != 0 ) {
+      result.insert(*it);
+    }
+  }
+  return result;
+}
+
 void printSet(set<int> S) {
   for (set<int>::iterator it = S.begin(); it != S.end(); it++) {
     cout << *it << " ";
@@ -30,7 +47,7 @@ int main() {
   for (int i=10; i<30; i++) {
     S2.insert(i);
   }
-  for (int i=0; i<20; i+=2) {
+  for (int i=0; i<30; i+=2) {
     S3.insert(i);
   }
   cout << "S1:" << endl;
@@ -39,6 +56,8 @@ int main() {
   printSet(S2);
   cout << "S3:" << endl;
   printSet(S3);
-  cout << "Method:" << endl;
+  cout << "intersectionWithoutThird:" << endl;
   printSet( intersectionWithoutThird(S1, S2, S3) );
+  cout << "WeirdIntersection:" << endl;
+  printSet( WeirdIntersection(S1, S2, S3) );
 }
