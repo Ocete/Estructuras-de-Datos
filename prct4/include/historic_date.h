808c1aaeb0736d2914efb7280858dc9f876080aa
Ocete <ocetebt@hotmail.com>
2017-11-25 20:50:10 +0100

    Modificado fecha historica a ingles

--- prct4/include/historic_date.h
+++ prct4/include/historic_date.h
@@ -1,17 +1,16 @@
 /**
- * @file fecha_historica.h
- * @brief Fichero cabecera de la fecha histórica.
+ * @file historic_date.h
+ * @brief Fichero cabecera de la date histórica.
  *
  * Se crea un int que almacena el año y un vector_dinamico<strings> para los eventos.
  *
  */
 
-#ifndef _FECHA_HISTORICA_H_
-#define _FECHA_HISTORICA_H_
+#ifndef _HITORIC_DATE_H_
+#define _HITORIC_DATE_H_
 
 #include <set>
 #include <string>
-#include "vector_dinamico.h"
 using namespace std;
 
 /**
@@ -19,66 +18,66 @@ using namespace std;
  *
  * Esta clase almacena un año, y un conjunto no ordenado de eventos.
  * Podemos, entre otras cosas, consultar el número de eventos, acceder a
- * uno específico o comprobar si contiene una clave (string) en alguno de sus eventos.
+ * uno específico o comprobar si contiene una key (string) en alguno de sus eventos.
  */
 
-class FechaHistorica{
+class HistoricDate{
 private:
-  int fecha; /**< Año que representa la fecha histórica. */
+  int date; /**< Año que representa la fecha histórica. */
   set<string> s; /**< Vector dinámico que almacena los eventos. */
   const char SEPARATOR = '#'; /**< Separador entre eventos al leer datos. */
 
 public:
 /**
   * @brief Constructor básico de la clase
-  * @param anio fecha en la que ocurren los eventos.
+  * @param year fecha en la que ocurren los eventos.
   */
-  FechaHistorica(int anio);
+  HistoricDate(int year);
 /**
   * @brief Constructor de copia. Hace una copia @e profunda.
-  * @param fh objeto a copiar.
+  * @param hf objeto a copiar.
   */
-  FechaHistorica(const FechaHistorica &fh);
+  HistoricDate(const HistoricDate &hf);
 /**
   * @brief Sobrecarga del operador de asignación. Hace una copia @e profunda.
-  * @param fh objeto a copiar.
+  * @param hf objeto a copiar.
   * @return Referencia al objeto copiado.
   */
-  FechaHistorica& operator = (const FechaHistorica &fh);
+  HistoricDate& operator = (const HistoricDate &hf);
 /**
   * @brief Sobrecarga del operador de comparación.
-  * @param fh Fecha a comprar.
+  * @param hf Fecha a comprar.
   * @return True si el @año es el mismo, independientemente de los eventos.
   */
-  bool operator == (FechaHistorica fh);
+  bool operator == (HistoricDate hf);
 /**
   * @brief Sobrecarga del operador de comparación menor.
-  * @param fh Fecha Historica a comprar.
+  * @param hf Fecha Historica a comprar.
   * @return True si el @e año es el menor, independientemente de los eventos.
   */
-  bool operator < (FechaHistorica fh);
+  bool operator < (HistoricDate hf);
 /**
   * @brief Sobrecarga del operador de comparación mayor.
-  * @param fh Fecha Historica a comprar.
+  * @param hf Fecha Historica a comprar.
   * @return True si el @e año es el mayor, independientemente de los eventos.
   */
-  bool operator > (FechaHistorica fh);
+  bool operator > (HistoricDate hf);
 /**
   * @brief Añade un evento a la Fecha Historica. Eficiencia: O(1).
-  * @param fh Fecha Historica a comprar.
+  * @param hf Fecha Historica a comprar.
   * @return True si el @e año es el mayor, independientemente de los eventos.
   */
-  void addEvento(string evento);
+  void addEvent(string event);
 /**
-  * @brief Añade los eventos de la Fecha Historica proporcionado como parámetro. Eficiencia: O(k), k = fh.getNumEventos()
-  * @param fh Fecha Historica a unir
+  * @brief Añade los eventos de la Fecha Historica proporcionado como parámetro. Eficiencia: O(k), k = hf.getNumEvents()
+  * @param hf Fecha Historica a unir
   */
-  void unir(const FechaHistorica &fh);
+  void merge(const HistoricDate &hf);
 /**
   * @brief Devuelve el número de eventos.
   * @return Número actual de eventos en la Fecha Historica.
   */
-  int getNumEventos() const;
+  int getNumEvents() const;
 /**
   * @brief Devuelve el año de la Fecha Histórica.
   * @return Año de la Fecha Historica.
@@ -89,18 +88,18 @@ public:
   */
   void print() const;
 /**
-  * @brief Comprueba si la Fecha Historica contiene la clave en alguno de sus eventos. Eficiencia: O(getNumEventos()*m), m = max{getEvento(i).size() : 0 <= i < getNumEventos() }
-  * @param clave string a buscar.
-  * @return True si la clave aparece en algún evento. False en caso contrario.
+  * @brief Comprueba si la Fecha Historica contiene la key en alguno de sus eventos. Eficiencia: O(getNumEvents()*m), m = max{getEvent(i).size() : 0 <= i < getNumEvents() }
+  * @param key string a buscar.
+  * @return True si la key aparece en algún evento. False en caso contrario.
   */
-  bool contieneClave(string clave) const;
+  bool includesKey(string key) const;
 /**
   * @brief Función amiga de la clase, lee una Fecha Historica a través del flujo.
   * @param is referencia al flujo de entrada por el que se leerá el objeto.
-  * @param fh referencia a un tipo Fecha Historica en la que se guardarán los datos.
+  * @param hf referencia a un tipo Fecha Historica en la que se guardarán los datos.
   * @return flujo proporcionado como argumento.
   */
-  friend istream& operator >> (istream& is, FechaHistorica &fh);
+  friend istream& operator >> (istream& is, HistoricDate &hf);
 };
 
 #endif
---
+++ prct4/src/historic_date.cpp
@@ -0,0 +1,90 @@
+#include "date_historica.h"
+#include <iostream>
+#include <sstream> // Para el flujo de entrada
+using namespace std;
+
+HistoricDate::HistoricDate(int year){
+  date = year;
+}
+
+HistoricDate::HistoricDate(const HistoricDate &hd) {
+  date = hd.date;
+  for (set<string>::iterator it=hd.s.begin(); it!=hd.s.end(); it++) {
+    s.insert(*it);
+  }
+}
+
+HistoricDate& HistoricDate::operator=(const HistoricDate &hd) {
+  if (this != &hd) {
+    s.clear();
+    date = hd.date;
+    for (set<string>::iterator it=hd.s.begin(); it!=hd.s.end(); it++) {
+      s.insert(*it);
+    }
+  }
+  return *this;
+}
+
+bool HistoricDate::operator==(HistoricDate hd) {
+  return date == hd.date;
+}
+
+bool HistoricDate::operator<(HistoricDate hd) {
+  return date < hd.date;
+}
+
+bool HistoricDate::operator>(HistoricDate hd) {
+  return date > hd.date;
+}
+
+
+void HistoricDate::addEvent(string event) {
+  s.insert(event);
+}
+
+// Si hay eventos dobles, set se foundarga de ellos
+void HistoricDate::merge(const HistoricDate &hd) {
+  for (set<string>::iterator it=hd.s.begin(); it!=hd.s.end(); it++) {
+    s.insert(*it);
+  }
+}
+
+int HistoricDate::getNumEventos() const {
+  return s.size();
+}
+
+int HistoricDate::getdate() const {
+  return date;
+}
+
+void HistoricDate::print() const {
+  cout << "Año " << date << ":" << endl;
+  for (set<string>::iterator it=hd.s.begin(); it!=hd.s.end(); it++) {
+    cout << "\t" << *it << endl;
+  }
+}
+
+bool HistoricDate::includesKey(string key) const {
+  bool found = false;
+  set<string>::iterator it = s.begin();
+  size_t pos;
+  while (!found && it != s.end()) {
+    pos = it->find(key);
+    found = pos != string::npos;
+    it++;
+  }
+  return found;
+}
+
+istream& operator >> (istream& is, HistoricDate &hd) {
+  string lecture;
+  getline(is, lecture, hd.SEPARATOR);
+  hd.date = stoi(lecture);
+
+  while( is.good() ) {
+      string event;
+      getline(is, event, hd.SEPARATOR);
+      hd.s.insert(event);
+  }
+  return is;
+}
