33585064ce887202b79093365c6ac9bfe781f21c
Yabir <yabirgb@gmail.com>
2017-11-25 11:57:51 +0100

    Añadidos archivos practica 4

---
+++ prct4/include/cronology.h
@@ -0,0 +1,14 @@
+#include <utility>      // std::pair, std::make_pair
+#include <string>       // std::string
+#include <set>
+
+template <class T>
+class HistoricDate{
+
+private:
+  std::pair< std::string, set<string> > event;
+public:
+
+
+
+};
